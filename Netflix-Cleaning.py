8fab43548bfb8fe6a187cf028ffc713832b3b5a4
Aaron Chatrath <ac12g15@gmail.com>
2022-06-26 19:58:58 +0100

    Splitting the Stars-cleaned column into two seperate columns

--- Netflix-Cleaning.py
+++ Netflix-Cleaning.py
@@ -22,6 +22,18 @@ df_pyspark_alter=df_pyspark_alter.withColumn("YEAR_cleaned",regexp_replace(col("
                 .withColumn("ONE-LINE-cleaned",regexp_replace(col("ONE-LINE"), regex_string_newline,"")) \
                 .withColumn("STARS-cleaned",regexp_replace(col("STARS"), regex_string_newline,""))
 
+#Splitting the 'Stars' original column into two columns - Director and Stars
+#Selecting the cleaned columns for the netflix table 
+split_cols = pyspark.sql.functions.split(df_pyspark_alter['STARS-cleaned'], 'Stars:')
+df_pyspark_alter = df_pyspark_alter.select("MOVIES", "RATING", "VOTES", "RunTime", "YEAR_cleaned","GENRE-cleaned","ONE-LINE-cleaned","STARS-cleaned",
+                split_cols.getItem(0).alias('Director'),
+                split_cols.getItem(1).alias('Stars'))
+
+print(df_pyspark_alter.show(10))
+
+#Dropping the original Stars column
+df_pyspark_alter = df_pyspark_alter.drop('STARS-cleaned')
+
 print(df_pyspark_alter.show(10))
 
 
